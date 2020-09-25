317a4d44d0269a1b9ec6e09c0aab9c78655ff613
999itsolution <999itsolution@gmail.com>
2020-09-25 14:44:21 +0600

    Custom Select Done

--- dist/js/main.js
+++ dist/js/main.js
@@ -18071,8 +18071,31 @@ $(document).ready(function () {
   $('#menu-bar').click(function () {
     $(this).toggleClass('change');
     $('.sidebar').toggleClass('sidebar-show');
+  }); // Custom Select
+
+  setDefaultOption();
+  $(".select__default").click(function () {
+    $(this).siblings('.select__option').toggleClass('show');
+    $(this).children('.arrow-down').toggleClass('rotate');
   });
-}); // Custom Select
+  $(".select__option li").click(function () {
+    $(this).parents('.select__option').children('li').removeClass('selected');
+    $(this).addClass('selected');
+    setDefaultOption();
+    $(this).parents('.select__option').removeClass('show');
+  });
+
+  function setDefaultOption() {
+    $('.select__option li').each(function () {
+      if ($(this).hasClass('selected')) {
+        var value = $(this).attr('data-value');
+        var html = $(this).html();
+        var arrow = "<img src=\"images/svgs/arrow-down.svg\" class=\"arrow-down\" alt=\"\"height=\"10px\">";
+        $(this).parents('.select').children('.select__default').html("<li class=\"option\" data-value=".concat(value, ">").concat(html, "</li> ").concat(arrow));
+      }
+    });
+  }
+});
 
 /***/ }),
 
--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,35 @@ $(document).ready(function(){
         $('.sidebar').toggleClass('sidebar-show')
     })
 
+    // Custom Select
+
+    setDefaultOption();
+
+    $(".select__default").click(function(){
+            $(this).siblings('.select__option').toggleClass('show');
+            $(this).children('.arrow-down').toggleClass('rotate');
+      })
+      
+    $(".select__option li").click(function(){
+        $(this).parents('.select__option').children('li').removeClass('selected');
+        $(this).addClass('selected');
+        setDefaultOption();
+        $(this).parents('.select__option').removeClass('show');
+    })
+
+    function setDefaultOption(){
+        $('.select__option li').each(function(){
+            if($(this).hasClass('selected')){
+                var value = $(this).attr('data-value');
+                var html  = $(this).html();
+                var arrow = `<img src="images/svgs/arrow-down.svg" class="arrow-down" alt=""height="10px">`;
+                $(this).parents('.select').children('.select__default').html(`<li class="option" data-value=${value}>${html}</li> ${arrow}`)
+            }
+        })
+    }
+
+
 });
 
 
-// Custom Select
+
